847cd9a0a963a4bb0eec3cbfc2f04bc74211641b
galan.wang <619531862@qq.com>
2018-12-17 14:25:50 +0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ app = new appKoa();
 
 
 // 静态文件serve在koa-router的其他规则之上 
-app.use(serve(path.resolve('dist'))); // 将webpack打包好的项目目录作为Koa静态文件服务的目录
+app.use(serve(path.resolve('../dist'))); // 将webpack打包好的项目目录作为Koa静态文件服务的目录
 
 app.use(require('koa-bodyparser')());
 
